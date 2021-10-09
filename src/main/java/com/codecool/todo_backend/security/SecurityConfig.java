52da8450c48b47115ea0c6a27f083309aa584e5b
Ani <jancso.anita@gmail.com>
2021-10-09 19:53:47 +0200

    Add JwtTokenServices and JwtTokenFilter

--- src/main/java/com/codecool/todo_backend/security/SecurityConfig.java
+++ src/main/java/com/codecool/todo_backend/security/SecurityConfig.java
@@ -7,14 +7,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
     }
 
     @Override
@@ -29,7 +30,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/api/v1/todo/**").authenticated() // allowed only when signed in
                 .antMatchers(HttpMethod.POST, "/api/v1/todo/**").authenticated() // allowed only when signed in
                 .antMatchers(HttpMethod.PUT, "/api/v1/todo/**").authenticated() // allowed only when signed in
-                .antMatchers(HttpMethod.DELETE, "/api/v1/removeAllCompleted/").hasRole("ADMIN") // allowed if signed in with ADMIN role
-                .anyRequest().denyAll(); // anything else is denied
+                .antMatchers(HttpMethod.DELETE, "/api/v1/todo/**").authenticated() // allowed only when signed in
+//                .antMatchers(HttpMethod.DELETE, "/api/v1/todo/removeAllCompleted/").hasRole("ADMIN") // allowed if signed in with ADMIN role
+                .anyRequest().denyAll() // anything else is denied
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
