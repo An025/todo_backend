8a137d01da7e5d08bfd47cea648adee93cf1313b
Ani <jancso.anita@gmail.com>
2021-10-09 18:43:52 +0200

    Create CustomUserDetailsService, UserCredentials and search user in database by username

--- src/main/java/com/codecool/todo_backend/repository/UserRepository.java
+++ src/main/java/com/codecool/todo_backend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.codecool.todo_backend.repository;
 import com.codecool.todo_backend.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
+    Optional<AppUser> findByUsername(String username);
 }
