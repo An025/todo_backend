a8be411b1852cff06f88478443915e36bea6aaab
Ani <jancso.anita@gmail.com>
2021-10-09 18:13:17 +0200

    Encrypting stored password

--- src/main/java/com/codecool/todo_backend/configuration/AppConfiguration.java
+++ src/main/java/com/codecool/todo_backend/configuration/AppConfiguration.java
@@ -2,13 +2,23 @@ package com.codecool.todo_backend.configuration;
 
 import com.codecool.todo_backend.model.AppUser;
 import com.codecool.todo_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfiguration {
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    public AppConfiguration() {
+        this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Bean
     @Profile("production")
@@ -17,7 +27,7 @@ public class AppConfiguration {
             AppUser test_user = AppUser.builder()
                     .username("test_user")
                     .email("test@test.com")
-                    .password("password")
+                    .password(passwordEncoder.encode("password"))
                     .build();
             userRepository.save(test_user);
         };
